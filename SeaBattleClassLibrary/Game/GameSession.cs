618bc185925dbe5e1f1d76b4cd38f44e3f66ed3e
Nikita Novikov <sirpointer@gmail.com>
2019-04-17 13:49:05 +0400

    .NETStandard

--- SeaBattleClassLibrary/Game/GameSession.cs
+++ SeaBattleClassLibrary/Game/GameSession.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeaBattleClassLibrary.Game
 {
@@ -55,5 +51,4 @@ namespace SeaBattleClassLibrary.Game
             return ip && name;
         }
     }
-
 }
