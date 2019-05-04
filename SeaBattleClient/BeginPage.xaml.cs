1f5b2b398a38c57a9105823791dc231d43f47e3e
Cactus <danilcheva.darja@yandex.ru>
2019-05-04 17:30:55 +0400

    beginPage

--- SeaBattleClient/BeginPage.xaml.cs
+++ SeaBattleClient/BeginPage.xaml.cs
@@ -20,6 +20,14 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 using SeaBattleClassLibrary.Game;
 using Game = SeaBattleClassLibrary.Game;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using static SeaBattleClient.JoinGamePage;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using SeaBattleClassLibrary.DataProvider;
+using System.Text;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -34,11 +42,21 @@ namespace SeaBattleClient
         {
             get
             {
-                return this.DataContext as GameField;
+                return Player?.GameField;
             }
         }
 
-        public Player player = null;
+        public Player Player
+        {
+            get
+            {
+                return this.DataContext as Player;
+            }
+            set
+            {
+                DataContext = value;
+            }
+        }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -47,11 +65,11 @@ namespace SeaBattleClient
                 return;
                 //player = e.Parameter as Player;
 
-            if (player == null)
+            if (Player == null)
             {
-                player = e.Parameter as Player;
+                Player = e.Parameter as Player;
                 
-                DataContext = player.GameField;
+                DataContext = Player.GameField;
                 
                 Image1.DataContext = Model.Ships[0];
                 Image2.DataContext = Model.Ships[1];
@@ -483,8 +501,175 @@ namespace SeaBattleClient
                 Canvas.SetTop(image, y);
                 Canvas.SetLeft(image, x + image.ActualWidth);
             }
+        }
 
 
+        public static ManualResetEvent pingDone = new ManualResetEvent(false);
+
+        // The response from the remote device.  
+        private static String response = String.Empty;
+        private static StateObject stateObject = new StateObject();
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            IPEndPoint remoteEP = Player.IPEndPoint;
+            //ring = new ProgressRing() { IsActive = true };
+            string fieldGame = Serializer<GameField>.SetSerializedObject(Model);
+
+            await Task.Run(() => {
+                pingDone.Reset();
+                //IPHostEntry ipHostInfo = Dns.GetHostEntry(ip);
+                //IPAddress ipAddress = ipHostInfo.AddressList.Where(x => x.AddressFamily == AddressFamily.InterNetwork).First();
+
+                // Create a TCP/IP socket.  
+                Socket client = new Socket(remoteEP.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                StateObject state = new StateObject();
+                state.workSocket = client;
+                state.obj = fieldGame;
+
+                // Connect to the remote endpoint.  
+                client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), state);
+                pingDone.WaitOne();
+            });
+
+
+
+
+            Answer.AnswerTypes dataType = GetAnswerType(response);
+            //string result = GetJsonRequestResult(content);
+            if (dataType == Answer.AnswerTypes.GameReady)
+            {
+                (Parent as Frame).Navigate(typeof(GamePage), Model);
+            } 
+            else
+            {
+                Receive(stateObject);
+            }
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                StateObject state = (StateObject)ar.AsyncState;
+                
+                Socket client = state.workSocket;
+
+                // Complete the connection.  
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+
+
+                JObject jObject = new JObject();
+                jObject.Add(JsonStructInfo.Type, Request.EnumTypeToString(Request.RequestTypes.SetField));
+                //string message = Serializer<BeginGame>.SetSerializedObject();
+                jObject.Add(JsonStructInfo.Result, state.obj.ToString());
+                //jObject.Add(JsonStructInfo.Result, message);
+
+                string s = jObject.ToString() + JsonStructInfo.EndOfMessage;
+
+                // Send test data to the remote device.  
+                Send(state, s);
+                // send ping
+
+                // Signal that the connection has been made.  
+                //connectDone.Set();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Send(StateObject state, String data)
+        {
+            Socket client = state.workSocket;
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.UTF8.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), state);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+                // Signal that all bytes have been sent.  
+                //sendDone.Set();
+                stateObject = state;
+                Receive(state);
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Receive(StateObject state)
+        {
+            try
+            {
+                Socket client = state.workSocket;
+                // Begin receiving the data from the remote device.  
+                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket   
+                // from the asynchronous state object.  
+                // Retrieve the state object and the client socket   
+                // from the asynchronous state object.  
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+
+                // Read data from the remote device.  
+                int bytesRead = client.EndReceive(ar);
+
+                //if (bytesRead > 0)
+                //{
+                //    // There might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.UTF8.GetString(state.buffer, 0, bytesRead));
+                // All the data has arrived; put it in response.  
+                if (state.sb.Length > 1)
+                {
+                    response = state.sb.ToString();
+                }
+                // Signal that all bytes have been received.  
+                //receiveDone.Set();
+
+                //client.Shutdown(SocketShutdown.Both);
+                //client.Close();
+
+                pingDone.Set();
+
+                //startPage.DeactivateRing();
+                //startPage.ring = new ProgressRing() { IsActive = false };
+                //startPage.progresRing.IsActive = false;
+                //(startPage.Parent as Frame).Navigate(typeof(CreateGamePage), startPage.Model);
+                //}
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
