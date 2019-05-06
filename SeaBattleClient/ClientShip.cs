b7ae6b8dfadb8880cdf4191bd7fdf97ff0b279a2
Cactus <danilcheva.darja@yandex.ru>
2019-05-06 21:06:39 +0400

    hren'

---
+++ SeaBattleClient/ClientShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeaBattleClassLibrary.Game;
+
+namespace SeaBattleClient
+{
+    class ClientShip : SeaBattleClassLibrary.Game.Ship
+    {
+        public ClientShip(int id, ShipClass shipClass = ShipClass.OneDeck, Orientation orientation = Orientation.Horizontal, Location location = null) : base(id, shipClass, orientation, location)
+        {
+        }
+    }
+}
--- SeaBattleClient/GamePage.xaml.cs
+++ SeaBattleClient/GamePage.xaml.cs
@@ -1,18 +1,25 @@
-﻿using SeaBattleClassLibrary.Game;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using SeaBattleClassLibrary.Game;
+using Game = SeaBattleClassLibrary.Game;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using static SeaBattleClient.JoinGamePage;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using SeaBattleClassLibrary.DataProvider;
+using System.Text;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -32,6 +39,7 @@ namespace SeaBattleClient
         }
 
         Player player = null;
+        private Windows.UI.Color backColor;
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -43,6 +51,84 @@ namespace SeaBattleClient
         public GamePage()
         {
             this.InitializeComponent();
+
+            //заполнить поле первого игрока
+            CreateField(Player1Grid);
+            FillFieldWithRectangle(Player1Grid);
+
+            foreach(var ship in Model.GameField.Ships)
+            {
+                Image image = new Image();
+                BitmapImage bitmapImage = new BitmapImage();
+                var uri = new Uri("ms-appx:///Assets/Ships/ship.jpg", UriKind.Absolute);
+                bitmapImage.UriSource = uri;
+                image.Source = bitmapImage;
+                image.Width = ship.ShipWidth*(Player1Grid.Width/10);
+                image.Height = ship.ShipHeight*(Player1Grid.Height/10);
+                Player1Grid.Children.Add(image);
+
+
+                Grid.SetColumnSpan(image, ship.Location.Size);
+                Grid.SetRow(image, ship.Location.Y);
+                Grid.SetColumn(image, ship.Location.X);
+            }
+            //добавление картинки на грид намертво
+            /*Image image = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            var uri = new Uri("ms-appx:///Assets/Ships/ship.jpg", UriKind.Absolute);
+            bitmapImage.UriSource = uri;
+            image.Source = bitmapImage;
+            image.Width = 90;
+            image.Height = 30;
+            FieldGrid.Children.Add(image);
+
+
+            Grid.SetColumnSpan(image, 3);
+            Grid.SetRow(image, row);
+            Grid.SetColumn(image, column);*/
+
+
+
+
+
+
+
+
+
+            //заполнить поле второго игрока
+            CreateField(Player2Grid);
+            FillFieldWithRectangle(Player2Grid);
+        }
+
+        private void FillFieldWithRectangle(Grid grid)
+        {
+            for (int i = 0; i < grid.RowDefinitions.Count; i++)
+            {
+                for (int j = 0; j < grid.ColumnDefinitions.Count; j++)
+                {
+                    Rectangle rectangle = new Rectangle() { StrokeThickness = 1 };
+                    rectangle.Stroke = new SolidColorBrush(Colors.Black);
+                    rectangle.Fill = new SolidColorBrush(backColor);
+                    rectangle.Name = i.ToString() + j.ToString();
+                    grid.Children.Add(rectangle);
+                    Grid.SetRow(rectangle, i);
+                    Grid.SetColumn(rectangle, j);
+                }
+            }
+        }
+
+        private void CreateField(Grid grid)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+
+        private void Player2Grid_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            
         }
     }
 }
